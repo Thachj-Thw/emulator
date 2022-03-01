842c807078ffecd4296795c42644cfa9ad21a09c
Thw <chanhosatoh@gmail.com>
2022-03-01 20:36:03 +0700

    rename package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='emulator',
+    name='emulator-Thw',
     version='0.0.1',
     description='package help control emulator',
     long_description=long_description,
